df5515fa92b1930530e9da5f2135bb6ba76493ce
Honeyy7 <honey51104@gmail.com>
2025-03-31 22:01:15 +0530

    scrape done

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .import views
+from . import views
 
 urlpatterns = [
-    path("search/",views.monument_search)
+    path("search/",views.monument_search),
+    path("data/", views.scrape_data, name='data'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
-# Create your views here.
+from bs4 import BeautifulSoup
+import requests
 
 def monument_search(request):
     return render(request, 'search_monument.html')
 
+def scrape_data(request):
+    url = "https://en.wikipedia.org/wiki/Taj_Mahal"
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.content, "html.parser")
+        
+        paragraphs = [ p.text.strip() for p in soup.find_all("p")]
+        
+        scraped_data = {
+            "paragraphs" : paragraphs,
+        }
+        
+        # print(scraped_data)
+        return render(request, "data.html",{"data": scraped_data})
+    except Exception as e:
+        erorrMessage = "Unexpected error"
+        return render(request, erorrMessage)
